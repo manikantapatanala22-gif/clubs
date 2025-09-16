5de5e27272b2d5ae58020a9df80d7f4a92ddd940
rich1ebytes <richardgomesxd@gmail.com>
2025-09-17 03:52:37 +0530

    changed title

--- frontend/src/components/TeamCard.jsx
+++ frontend/src/components/TeamCard.jsx
@@ -9,16 +9,16 @@ const TeamCard = ({ name, title, tagline, image, instagram }) => {
           className="w-full h-full object-cover"
         />
       </div>
-      <h3 className="text-xl font-bold text-brand-offwhite">{name}</h3>
-      <p className="text-sm font-semibold text-brand-offwhite2 mb-1">{title}</p>
+      <h3 className="text-2xl font-bold text-brand-offwhite">{name}</h3>
+      <p className="text-l font-semibold text-brand-offwhite2 mb-1">{title}</p>
       <p className="text-sm italic text-brand-offwhite2 mb-4">{tagline}</p>
       {instagram && (
         <a
           href={instagram}
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-brand-accent text-white py-2 px-4 rounded-full font-semibold hover:bg-white hover:text-brand-accent transition-colors duration-200">
-          View Instagram
+          className="bg-brand-accent text-white py-2 px-4 rounded-full font-bold hover:bg-white hover:text-brand-accent transition-colors duration-200">
+           !nstagram
         </a>
       )}
     </div>
--- frontend/src/data/teamData.js
+++ frontend/src/data/teamData.js
@@ -8,42 +8,36 @@ import Sahithi from "../assets/team/sahithi.png";
 const teamsData = [
   {
     name: "Richard Gomes",
-    title: "Fake CEO 🥵",
     tagline: "Surinder The Bottle",
     image: Richie,
     instagram: "https://www.instagram.com/richxiee/",
   },
   {
     name: "Deva Vrata",
-    title: "Fake CTO 😎",
     tagline: "Surinder Evaru?",
     image: Deva,
     instagram: "https://www.instagram.com/redcreeper19/",
   },
   {
     name: "Vishnu",
-    title: "Java System Designer🤫",
     tagline: "Tagline cannot contain his genius",
     image: Vishnu,
     instagram: "https://www.instagram.com/spidermiles120/",
   },
   {
     name: "Manikanta",
-    title: "Kamal Hassan😛",
     tagline: "Chick Magnet",
     image: Manikanta,
     instagram: "https://www.instagram.com/maniii_ii2/",
   },
   {
     name: "Rishi Shankar",
-    title: "Canva Developer",
     tagline: "Chick Magnet + Pullihora",
     image: Rishi,
     instagram: "https://www.instagram.com/rishi.____.05/",
   },
   {
     name: "Sahithi Manda",
-    title: "Morale Support",
     tagline: "Rich people",
     image: Sahithi,
     instagram: "https://www.instagram.com/_.sahithigoud._/",
